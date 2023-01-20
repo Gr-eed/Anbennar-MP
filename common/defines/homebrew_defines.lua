bbb438a423349a47e49479577c65f57b3cde8ed3
Gr-eed <123059430+Gr-eed@users.noreply.github.com>
2023-01-20 14:54:39 -0500

    Add files via upload

---
+++ common/defines/homebrew_defines.lua
@@ -0,0 +1,12 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NMilitary.SLACKEN_AP_DROP = 0.12
+NDefines.NCountry.STATE_MAINTENANCE_DEV_FACTOR = 0.012	-- +50%~
+NDefines.NCountry.IDEA_TO_TECH = -0.01
+NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.35 -- from 1-1-0 to 1-1-1
+NDefines.NCountry.MAX_ACTIVE_POLICIES = 8	-- from 5, go crazy!
+NDefines.NEconomy.LAND_TECH_MAINTENANCE_IMPACT = 0.025 -- +25%
+NDefines.NEconomy.NAVAL_TECH_MAINTENANCE_IMPACT = 0.025 -- +25%
+
